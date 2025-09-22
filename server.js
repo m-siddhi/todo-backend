75df70c1ca4bd1e32bec63ab653e3713f9e72f19
Siddhi <manjrekarsiddhi72@gmail.com>
2025-09-22 15:32:49 +0530

    server.js changed

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.json());
 app.use(cors());
 if (process.env.NODE_ENV !== "production") app.use(morgan("dev"));
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use("/api/tasks", taskRoutes);
 
 app.use(notFound);
