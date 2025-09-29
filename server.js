d92ebcee26eeba82ae9b1ae621a40c16840a7ba2
Siddhi <manjrekarsiddhi72@gmail.com>
2025-09-29 16:57:17 +0530

    server port changed

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.get("/", (req, res) => {
   res.send("To-Do List API is running!");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
